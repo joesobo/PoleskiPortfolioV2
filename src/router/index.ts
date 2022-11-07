716d2194f27379f645825540666db2d1fed98f9a
Joe <soboleskijoe@gmail.com>
2022-11-06 19:20:00 -0800

    reorg page index

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
+import Notes from '../views/Notes.vue'
+import Projects from '../views/Projects.vue'
+import NewTab from '../views/NewTab.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,26 +20,27 @@ const router = createRouter({
 			// route level code-splitting
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import('../views/About.vue'),
+			component: About,
 		},
 		{
 			path: '/contact',
 			name: 'contact',
-			component: () => import('../views/Contact.vue'),
+			component: Contact,
 		},
 		{
 			path: '/notes',
 			name: 'notes',
-			component: () => import('../views/Notes.vue'),
+			component: Notes,
 		},
 		{
 			path: '/projects',
 			name: 'projects',
-			component: () => import('../views/Projects.vue'),
-		},{
+			component: Projects,
+		},
+		{
 			path: '/newTab',
 			name: 'newTab',
-			component: () => import('../views/NewTab.vue'),
+			component: NewTab,
 		},
 	],
 })
